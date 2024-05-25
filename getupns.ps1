89da228ece872dc88e8304ce0a247898981ade73
SleepOnFirewood <50755859+SleepOnFirewood@users.noreply.github.com>
2024-05-25 15:27:00 -0400

    Add files via upload

---
+++ getupns.ps1
@@ -0,0 +1,39 @@
+﻿$servers = @("domain names")
+
+ 
+
+Get-Content "C:\Users\_\Documents\ids.txt" |
+
+ 
+
+ForEach-Object {
+
+ 
+
+    foreach ($server in $servers) {
+
+ 
+
+        $aduser = Get-ADUser -Filter "SamAccountName -eq '$_'" -Properties Name, Mail,UserPrincipalName -Server $server | Select-Object UserPrincipalName
+
+ 
+
+        if ($aduser) {
+
+ 
+
+            $aduser # send object down the pipeline
+
+ 
+
+            break   # break out of server loop and move to next user
+
+ 
+
+        }
+
+    }
+
+ 
+
+} | Export-Csv 'C:\temp\testemail.csv' -NoTypeInformation
