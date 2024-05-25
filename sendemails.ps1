254c0197e5110e4237b0d054dd1ab45a3993e213
SleepOnFirewood <50755859+SleepOnFirewood@users.noreply.github.com>
2024-05-25 15:24:35 -0400

    Add files via upload
    Add powershell script that sends emails en masse with the use of ordered lists.

---
+++ sendemails.ps1
@@ -0,0 +1,65 @@
+﻿$mailme = @(test@test.com)
+
+ 
+
+ 
+
+$urls = @("")
+
+$appNames = @("","","","")
+
+ 
+
+ 
+
+$status = (get-process | ? {$_.processname -match 'Outlook'})
+
+$outlook = new-object -comobject outlook.application
+
+ 
+
+ 
+
+for($i =0; $i -le $mailme.Length-1; $i++)
+
+{
+
+ 
+
+    $theUrls = $urls[$i].split(",")
+
+    $allUrls = ""
+
+    foreach ($g in $theUrls)
+
+    {
+
+    $allUrls += "`n$g"
+
+    }
+
+ 
+
+    $email = $outlook.CreateItem(0)
+
+    $email.To = $mailme[$i]
+
+    $email.Subject = "REVISION: Bypassed " +$appNames[$i] +" Urls"
+
+    $email.Body = "this is my email body" + $allUrls + "  "
+
+ 
+
+    $email.Cc = "test@test.com; test@test.com"
+
+ 
+
+    $email.Send()
+
+    IF($status){break}
+
+        else{$outlook.Quit()}
+
+   
+
+}
